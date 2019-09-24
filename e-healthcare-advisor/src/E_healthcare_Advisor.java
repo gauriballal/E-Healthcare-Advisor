0423080b47ab5dc84d71a1540fd399bc821b5af2
gauriballal <gauri.ballal16@gmail.com>
2019-09-24 11:47:20 +0530

    #registration of a patient successful

--- e-healthcare-advisor/src/E_healthcare_Advisor.java
+++ e-healthcare-advisor/src/E_healthcare_Advisor.java
@@ -1,20 +1,20 @@
 import java.sql.*;
-public class E_healthcare_Advisor {
+
+public class E_healthcare_Advisor 
+{
 	public static void main(String args[])
 	{
 		try
 		{
-			Connection con;
+			Connection con = null;
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/e_healthcare_advisor?user=root&password=admin&useSSL=false");
-		        System.out.println("database connectivity successful");
-			conn.close();
+			Connection con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/e_healthcare_advisor?user=root&password=admin&useSSL=false");
+		    System.out.println("database connectivity successful");
 		}
 		catch(Exception e)
 		{
 			System.out.println(e);
 		}
-
 		
 	}
 }
